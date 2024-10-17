f911eb05225b68ed2c7693e39aa5e455ca626f96
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-10-17 23:30:23 +0300

    #13 создал таблицу Playlists

--- Audio_player.DAL/AppDbContext.cs
+++ Audio_player.DAL/AppDbContext.cs
@@ -10,6 +10,8 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     public DbSet<AppUser> AppUsers { get; set; }
     public DbSet<AppUserRole> AppUserRoles { get; set; }
     public DbSet<UserProfile> UserProfiles { get; set; }
+    public DbSet<Playlist> Playlists { get; set; }
+    public DbSet<UserPlaylist> UserPlaylists { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
