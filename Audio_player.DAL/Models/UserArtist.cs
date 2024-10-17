d1eade200b8a806658ce6ecab3177115507ebabd
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-10-17 23:40:54 +0300

    #15 создал таблицу Artists

---
+++ Audio_player.DAL/Models/UserArtist.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Audio_player.DAL.Models;
+
+public class UserArtist
+{
+    public long UserId { get; set; }
+    public long ArtistId { get; set; }
+    public UserProfile? User {  get; set; }
+    public Artist? Artist { get; set; }
+}
+
+public class UserArtistConfig : IEntityTypeConfiguration<UserArtist>
+{
+    public void Configure(EntityTypeBuilder<UserArtist> builder)
+    {
+        builder.HasKey(x => new { x.UserId, x.ArtistId });
+    }
+}
