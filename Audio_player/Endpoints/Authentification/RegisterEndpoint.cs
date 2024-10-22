705364b9e5098b6a68c57a4d55d8ab53aaa534d3
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-10-23 02:09:56 +0300

    #32 создал endpoint

---
+++ Audio_player/Endpoints/Authentification/RegisterEndpoint.cs
@@ -0,0 +1,51 @@
+﻿using Audio_player.AppSettingsOptions;
+using Audio_player.DAL;
+using Audio_player.Helpers;
+using Audio_player.Models.Requests;
+using Audio_player.Models.Responses;
+using FastEndpoints;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Audio_player.Endpoints.Authentification;
+
+public class RegisterEndpoint(AppDbContext appDbContext, GenerateTokenHelper tokenHelper) : Endpoint<RegisterRequest, TokenResponse>
+{
+    private readonly GenerateTokenHelper _tokenHelper = tokenHelper;
+    private readonly AppDbContext _appDbContext = appDbContext;
+
+    public override void Configure()
+    {
+        Post("register");
+        Group<AuthentificationGroup>();
+        AllowAnonymous();
+    }
+
+    public override async Task<TokenResponse> ExecuteAsync(RegisterRequest req, CancellationToken ct)
+    {
+        var user = _appDbContext.AppUsers.Add(new DAL.Models.AppUser
+        {
+            Email = req.Email,
+            Password = req.Password,
+            UserProfile = new DAL.Models.UserProfile
+            {
+                Birthdate = req.Birthday,
+                Name = req.Name,
+                Surname = req.Surname
+            }
+        });
+
+        await _appDbContext.SaveChangesAsync(ct);
+
+        var accessToken = _tokenHelper.GenerateAccessToken(user.Entity.Email);
+
+        await _tokenHelper.SetRefreshTokenCookieAsync(HttpContext.Response, user.Entity.Email, ct);
+
+        return new TokenResponse
+        {
+            AccessToken = accessToken
+        };
+    }
+}
---
+++ Audio_player/Models/Requests/RegisterRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Audio_player.Models.Requests;
+
+public class RegisterRequest
+{
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string? Name { get; set; } 
+    public string? Surname { get; set; }
+    public DateOnly? Birthday { get; set; }
+}
