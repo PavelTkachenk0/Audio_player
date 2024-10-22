693fb2bc4cbd2556afdf033a3b550902140436e8
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-10-23 02:14:12 +0300

    #31 создал endpoint

---
+++ Audio_player/Endpoints/Authentification/LoginEndpoint.cs
@@ -0,0 +1,40 @@
+﻿using Audio_player.AppSettingsOptions;
+using Audio_player.DAL;
+using Audio_player.Helpers;
+using Audio_player.Models.Requests;
+using Audio_player.Models.Responses;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Audio_player.Endpoints.Authentification;
+
+public class LoginEndpoint(AppDbContext appDbContext, GenerateTokenHelper tokenHelper) : Endpoint<LoginRequest, TokenResponse>
+{
+    private readonly GenerateTokenHelper _tokenHelper = tokenHelper;
+    private readonly AppDbContext _appDbContext = appDbContext;
+
+    public override void Configure()
+    {
+        Post("login");
+        Group<AuthentificationGroup>();
+        AllowAnonymous();
+    }
+
+    public override async Task<TokenResponse> ExecuteAsync(LoginRequest req, CancellationToken ct)
+    {
+        var user = await _appDbContext.AppUsers.SingleAsync(x => x.Email == req.Email, ct);
+
+        var accessToken = _tokenHelper.GenerateAccessToken(user.Email);
+
+        await _tokenHelper.SetRefreshTokenCookieAsync(HttpContext.Response, user.Email, ct);
+
+        return new TokenResponse
+        {
+            AccessToken = accessToken
+        };
+    }
+}
