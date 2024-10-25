9129360f0e5d5259a49ec7303f17b1cf14679cea
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-10-25 11:43:45 +0300

    #44 создал модели

---
+++ Audio_player/Models/DTOs/GenreDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Audio_player.Models.DTOs;
+
+public class GenreDTO
+{
+    public short Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string CoverPath { get; set; } = null!;
+}
