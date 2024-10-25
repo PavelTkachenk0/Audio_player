0634cc075ea6ba646dbe7d7a3b5005fb2cd5e750
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-10-25 14:44:00 +0300

    #45 создал endpoint

---
+++ Audio_player/Endpoints/Genres/CreateGenreEndpoint.cs
@@ -0,0 +1,49 @@
+﻿using Audio_player.AppSettingsOptions;
+using Audio_player.Constants;
+using Audio_player.DAL;
+using Audio_player.Models.Requests;
+using FastEndpoints;
+using Microsoft.Extensions.Options;
+
+namespace Audio_player.Endpoints.Genres;
+
+public class CreateGenreEndpoint(AppDbContext appDbContext, IOptionsSnapshot<ImageStoreOptions> options) : Endpoint<CreateGenreRequest>
+{
+    private readonly ImageStoreOptions _options = options.Value;
+    private readonly AppDbContext _appDbContext = appDbContext;
+
+    public override void Configure()
+    {
+        Post("");
+        Group<GenreGroup>();
+        AllowFileUploads();
+        Policies(PolicyNames.HasAdminRole);
+    }
+
+    public async override Task HandleAsync(CreateGenreRequest req, CancellationToken ct)
+    {
+        if (!Directory.Exists(_options.FilesPath))
+        {
+            Directory.CreateDirectory(_options.FilesPath);
+        }
+
+        foreach (var file in Files)
+        {
+            var fileName = file.FileName;
+            var filePath = Path.Combine(_options.FilesPath, fileName);
+
+            using var fileStream = File.Create(filePath);
+
+            await file.CopyToAsync(fileStream, ct);
+
+            _appDbContext.Genres.Add(new DAL.Models.Genre
+            {
+                Name = req.Name.ToLower(),
+                CoverPath = filePath
+            });
+        }
+
+        await _appDbContext.SaveChangesAsync(ct);
+        await SendOkAsync(ct);
+    }
+}
