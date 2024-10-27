850cc1dea7a2fb543f334c69db10ac044ed9d7dc
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-10-27 16:01:49 +0300

    #50 изменил концепцию валидации

--- Audio_player/Validators/Files/PutGenreValidator.cs
+++ Audio_player/Validators/Files/PutGenreValidator.cs
@@ -3,11 +3,18 @@ using FluentValidation;
 
 namespace Audio_player.Validators.Files;
 
-public class PutGenreValidator : BasePostImageValidator<EditGenreRequest>
+public class PutGenreValidator : BaseFileValidator<EditGenreRequest>
 {
     public PutGenreValidator() : base()
     {
         RuleFor(x => x.Name)
             .NotEmpty();
+
+        RuleFor(x => x.Cover)
+            .Cascade(CascadeMode.Stop)
+            .Must(CheckExtensions!).When(x => x.Cover != null)
+            .WithMessage("incorrect_file_extension")
+            .Must(CheckFilesLenght!).When(x => x.Cover != null)
+            .WithMessage("cover_is_too_large");
     }
 }
