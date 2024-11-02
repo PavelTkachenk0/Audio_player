8465a913ee8b3e71e7080f7fc66c1b0bfa78f050
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-11-03 00:55:33 +0300

    #58 добавил метод удаления альбомов

---
+++ Audio_player/Endpoints/Albums/DeleteAlbumEndpoint.cs
@@ -0,0 +1,40 @@
+﻿using Audio_player.Constants;
+using Audio_player.DAL;
+using Audio_player.Services;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Audio_player.Endpoints.Albums;
+
+public class DeleteAlbumEndpoint(AppDbContext appDbContext, FileService fileService) : EndpointWithoutRequest
+{
+    private readonly AppDbContext _appDbContext = appDbContext;
+    private readonly FileService _fileService = fileService;
+
+    public override void Configure()
+    {
+        Delete("{id:int}");
+        Group<AlbumGroup>();
+        Policies(PolicyNames.HasAdminRole);
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var id = Route<long>("id");
+
+        var album = await _appDbContext.Albums.SingleOrDefaultAsync(x => x.Id == id, ct);
+
+        if (album == null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+
+        _appDbContext.Remove(album);
+
+        _fileService.DeleteFile(album.CoverPath);
+
+        await _appDbContext.SaveChangesAsync(ct);
+        await SendOkAsync(ct);
+    }
+}
