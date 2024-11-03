2fb05162a04a0b65692d0ebd4d904133e151ee4a
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-11-03 23:41:59 +0300

    #61 создал валидатор

---
+++ Audio_player/Validators/Tracks/PostTrackValidator.cs
@@ -0,0 +1,40 @@
+﻿using Audio_player.Models.Requests;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Audio_player.Validators.Tracks;
+
+public class PostTrackValidator : BaseAudioFileValidator<PostTrackRequest>
+{
+    public PostTrackValidator() : base()
+    {
+        RuleFor(x => x.SongName)
+            .NotEmpty();
+
+        RuleFor(x => x.AlbumId)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .MustAsync(async (val, ct) => await DbContext.Albums.AnyAsync(x => x.Id == val, ct))
+            .WithMessage("album_not_found");
+
+        RuleFor(x => x.GenreIds)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .MustAsync(async (val, ct) => await DbContext.Genres.CountAsync(g => val.Contains(g.Id), ct) == val.Count)
+            .WithMessage("one_or_more_genres_not_found");
+
+        RuleFor(x => x.ArtistIds)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .MustAsync(async (val, ct) => await DbContext.Artists.CountAsync(a => val.Contains(a.Id), ct) == val.Count)
+            .WithMessage("one_or_more_artists_not_found");
+
+        RuleFor(x => x.SongFile)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .Must(CheckExtensions)
+            .WithMessage("incorrect_file_extension")
+            .Must(CheckFilesLenght)
+            .WithMessage("cover_is_too_large");
+    }
+}
