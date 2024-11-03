ab66e7e4fcb3a242a48cd06debac803247d6e0f6
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-11-04 00:48:23 +0300

    #62 создал request

---
+++ Audio_player/Models/Requests/PutTrackRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Audio_player.Models.Requests;
+
+public class PutTrackRequest
+{
+    public string SongName { get; set; } = null!;
+    public IFormFile? SongFile { get; set; } 
+    public long AlbumId { get; set; }
+    public List<long> ArtistIds { get; set; } = [];
+    public List<short> GenreIds { get; set; } = [];
+    public int Duration { get; set; }
+}
