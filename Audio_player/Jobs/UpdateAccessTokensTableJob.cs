e25aafaaf702382e55705edbaeef93f677ab9ab0
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-11-03 12:18:00 +0300

    #83 создал Job's

---
+++ Audio_player/Jobs/UpdateAccessTokensTableJob.cs
@@ -0,0 +1,21 @@
+﻿using Audio_player.DAL;
+using Audio_player.Helpers;
+using Microsoft.EntityFrameworkCore;
+using Quartz;
+
+namespace Audio_player.Jobs;
+
+public class UpdateAccessTokensTableJob(AppDbContext appDbContext) : IJob
+{
+    private readonly AppDbContext _appDbContext = appDbContext;
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        await JobSynchronizationHelper.RunWithLock(async () =>
+        {
+            var tokens = _appDbContext.AccessTokens.Where(x => x.ExpiryDate < DateTime.UtcNow && !x.IsRevoked);
+
+            await tokens.ExecuteUpdateAsync(x => x.SetProperty(x => x.IsRevoked, true));
+        });
+    }
+}
