18be117214a3492a5bbf627ea640f430fd570972
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-11-03 12:17:43 +0300

    #83 добавил механизм блокировки общих ресурсов

---
+++ Audio_player/Helpers/JobSynchronizationHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Audio_player.Helpers;
+
+public class JobSynchronizationHelper
+{
+    private static readonly SemaphoreSlim semaphore = new(1, 1);
+
+    public static async Task RunWithLock(Func<Task> jobFunction)
+    {
+        await semaphore.WaitAsync();
+
+        try
+        {
+            await jobFunction();
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+}
