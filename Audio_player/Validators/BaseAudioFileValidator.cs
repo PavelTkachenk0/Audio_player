85fbe92e8740963828425a12afdd6ab85bbc7ac0
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-11-03 23:41:49 +0300

    #61 cоздал BaseAudioFileValidator

---
+++ Audio_player/Validators/BaseAudioFileValidator.cs
@@ -0,0 +1,23 @@
+﻿namespace Audio_player.Validators;
+
+public abstract class BaseAudioFileValidator<TRequest> : BaseValidator<TRequest>
+    where TRequest : class
+{
+    protected virtual bool CheckExtensions(IFormFile file)
+    {
+        var extensions = Configuration.GetSection("AudioStore:Extensions").Get<string[]>();
+
+        var success = extensions!.Contains(Path.GetExtension(file.FileName).ToLower());
+
+        return success;
+    }
+
+    protected virtual bool CheckFilesLenght(IFormFile file)
+    {
+        var maxSize = Configuration.GetSection("AudioStore:MaxSize").Get<int>();
+
+        var success = maxSize > file.Length;
+
+        return success;
+    }
+}
