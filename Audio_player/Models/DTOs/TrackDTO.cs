1865b89c3c9bb08e4a3e30ded067003ed756e288
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-11-03 13:27:35 +0300

    #59 добавил модели

---
+++ Audio_player/Models/DTOs/TrackDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Audio_player.Models.DTOs;
+
+public class TrackDTO
+{
+    public long Id { get; set; }
+    public string SongName { get; set; } = null!;
+    public string SongPath { get; set; } = null!;
+    public long ListeningCount { get; set; }
+    public int Duration { get; set; }
+    public bool IsFavorite { get; set; }
+    public List<ShortArtistDTO> Artists { get; set; } = [];
+    public ShortAlbumDTO Album { get; set; } = null!;
+    public List<ShortGenreDTO> Genres { get; set; } = [];
+}
