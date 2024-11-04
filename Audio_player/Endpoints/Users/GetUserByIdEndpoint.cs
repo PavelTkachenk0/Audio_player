8dcdc1ea0daf57279e5d9cc0bd114b1246b0fb7d
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-11-04 19:41:53 +0300

    #65 создал метод получения пользователя по id

---
+++ Audio_player/Endpoints/Users/GetUserByIdEndpoint.cs
@@ -0,0 +1,41 @@
+﻿using Audio_player.Constants;
+using Audio_player.DAL;
+using Audio_player.Models.DTOs;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Audio_player.Endpoints.Users;
+
+public class GetUserByIdEndpoint(AppDbContext appDbContext) : EndpointWithoutRequest<UserDTO?>
+{
+    private readonly AppDbContext _appDbContext = appDbContext;
+
+    public override void Configure()
+    {
+        Get("{id:int}");
+        Group<UserGroup>();
+        Policies(PolicyNames.HasAdminRole);
+    }
+
+    public override async Task<UserDTO?> ExecuteAsync(CancellationToken ct)
+    {
+        var id = Route<long>("id");
+
+        var user = await _appDbContext.AppUsers.Select(x => new UserDTO
+        {
+            Id = x.Id,
+            Email = x.Email,
+            Birthday = x.UserProfile!.Birthdate,
+            Surname = x.UserProfile!.Surname,
+            Name = x.UserProfile!.Name,
+        }).SingleOrDefaultAsync(x => x.Id == id);
+
+        if (user == null)
+        {
+            await SendNotFoundAsync(ct);
+            return null;
+        }
+
+        return user;
+    }
+}
