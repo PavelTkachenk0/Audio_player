a3848582c9531c6562880fbf9a9ed314e090ca8a
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-11-04 19:34:51 +0300

    #64 создал модели

---
+++ Audio_player/Models/DTOs/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Audio_player.Models.DTOs;
+
+public class UserDTO
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public DateOnly? Birthday { get; set; }
+    public string Email { get; set; } = null!;
+}
