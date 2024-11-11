18e73b1a96746aca26c2cfc7d69e3f2d8defd4af
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-11-11 20:27:12 +0300

    #101 создал модели

---
+++ Audio_player/Models/DTOs/SearchDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Audio_player.Models.DTOs;
+
+public class SearchDTO
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Source { get; set; } = null!;
+    public string CoverPath { get; set; } = null!;
+}
