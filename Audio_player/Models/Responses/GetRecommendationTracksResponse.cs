709fd9b9e466a1ab116126a6961102982b5daf21
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-11-12 15:23:37 +0300

    #104 создал модель

---
+++ Audio_player/Models/Responses/GetRecommendationTracksResponse.cs
@@ -0,0 +1,8 @@
+﻿using Audio_player.Models.DTOs;
+
+namespace Audio_player.Models.Responses;
+
+public class GetRecommendationTracksResponse : BaseListResponse<TrackDTO>
+{
+    public long TotalCount { get; set; }
+}
