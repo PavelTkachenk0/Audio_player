f5aebe06abcf38fbbdea87d3b891c3d29597dca4
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-11-15 18:26:54 +0300

    #106 создал метод добавления треков в избранное

---
+++ Audio_player/Endpoints/Favorites/FavoriteTracks/PostTrackToFavoriteEndpoint.cs
@@ -0,0 +1,53 @@
+﻿using Audio_player.Constants;
+using Audio_player.DAL;
+using Audio_player.Endpoints.Favorites.Tracks;
+using Audio_player.Models.Responses;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace Audio_player.Endpoints.Favorites.FavoriteTracks;
+
+public class PostTrackToFavoriteEndpoint(AppDbContext appDbContext) : EndpointWithoutRequest<FavoriteResponse>
+{
+    private readonly AppDbContext _appDbContext = appDbContext;
+
+    public override void Configure()
+    {
+        Post("{id:int}");
+        Group<FavoriteTracksGroup>();
+    }
+
+    public override async Task<FavoriteResponse> ExecuteAsync(CancellationToken ct)
+    {
+        var trackId = Route<long>("id");
+
+        if (!_appDbContext.Songs.Any(x => x.Id == trackId))
+        {
+            await SendNotFoundAsync(ct);
+            return new FavoriteResponse
+            {
+                Added = false
+            };
+        }
+
+        var email = HttpContext.User.Claims.
+          FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value!;
+        var userId = await _appDbContext.AppUsers.Where(x => x.Email == email)
+                .Select(x => x.UserProfile!.Id)
+                .SingleOrDefaultAsync(ct);
+
+        _appDbContext.UserSongs.Add(new DAL.Models.UserSongs
+        {
+            SongId = trackId,
+            UserId = userId
+        });
+
+        await _appDbContext.SaveChangesAsync(ct);
+
+        return new FavoriteResponse
+        {
+            Added = true
+        };
+    }
+}
