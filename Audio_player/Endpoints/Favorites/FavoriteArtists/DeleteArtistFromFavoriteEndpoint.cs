ae4a0622798facc4fa6fde54497d832ceef64f61
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-11-15 18:35:33 +0300

    #111 создал метод удаления артистов из избранного

---
+++ Audio_player/Endpoints/Favorites/FavoriteArtists/DeleteArtistFromFavoriteEndpoint.cs
@@ -0,0 +1,50 @@
+﻿using Audio_player.Constants;
+using Audio_player.DAL;
+using Audio_player.Models.Responses;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace Audio_player.Endpoints.Favorites.FavoriteArtists;
+
+public class DeleteArtistFromFavoriteEndpoint(AppDbContext appDbContext) : EndpointWithoutRequest<FavoriteResponse>
+{
+    private readonly AppDbContext _appDbContext = appDbContext;
+
+    public override void Configure()
+    {
+        Delete("{id:int}");
+        Group<FavoriteArtistsGroup>();
+    }
+
+    public override async Task<FavoriteResponse> ExecuteAsync(CancellationToken ct)
+    {
+        var artistId = Route<long>("id");
+
+        var email = HttpContext.User.Claims.
+          FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value!;
+        var userId = await _appDbContext.AppUsers.Where(x => x.Email == email)
+                .Select(x => x.UserProfile!.Id)
+        .SingleOrDefaultAsync(ct);
+
+        var artist = await _appDbContext.UserArtists.SingleOrDefaultAsync(x => x.UserId == userId && x.ArtistId == artistId, ct);
+
+        if (artist == null)
+        {
+            await SendNotFoundAsync(ct);
+            return new FavoriteResponse
+            {
+                Added = false
+            };
+        }
+
+        _appDbContext.UserArtists.Remove(artist);
+
+        await _appDbContext.SaveChangesAsync(ct);
+
+        return new FavoriteResponse
+        {
+            Added = true
+        };
+    }
+}
