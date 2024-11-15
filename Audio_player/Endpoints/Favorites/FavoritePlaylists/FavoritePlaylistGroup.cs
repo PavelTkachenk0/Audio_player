9409dd245f2e81d61687291248378acb7967075d
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2024-11-15 18:36:57 +0300

    #112 создал метод добавления плейлиста в избранное

---
+++ Audio_player/Endpoints/Favorites/FavoritePlaylists/FavoritePlaylistGroup.cs
@@ -0,0 +1,11 @@
+﻿using FastEndpoints;
+
+namespace Audio_player.Endpoints.Favorites.FavoritePlaylists;
+
+public class FavoritePlaylistGroup : SubGroup<FavoritesGroup>
+{
+    public FavoritePlaylistGroup()
+    {
+        Configure("playlists", ep => { });
+    }
+}
